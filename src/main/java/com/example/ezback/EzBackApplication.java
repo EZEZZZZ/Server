0c1fc8a37b02575ca150d1fea5a5d1eb3fc0b31e
randirao <randirao4245@gmail.com>
2025-12-06 22:45:12 +0900

    chore: 초기 커밋

---
+++ src/main/java/com/example/ezback/EzBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ezback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzBackApplication.class, args);
+    }
+
+}
